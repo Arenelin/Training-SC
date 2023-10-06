d67b20debcc6e262628a01c83a97caa840552b22
Arenelin <belov.1098@mail.ru>
2023-10-06 14:23:18 +0700

    setting up packages and creating the basic structure of the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Wrapper>
+        <Box>
+
+        </Box>
+      </Wrapper>
     </div>
   );
 }
 
+const Wrapper = styled.div`
+  background-color: #F5F5F5;
+`
+const Box = styled.div`
+max-width:1250px;
+gap: 10px;
+`
 export default App;
